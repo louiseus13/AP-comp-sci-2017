5bdfb695db9730cff3ad7948786e2d9bc9f2275c
louiseus13 <llalescu@pvlearners.net>
2017-05-23 23:48:10 -0700

    first commit

---
+++ pythonpractice.py
@@ -0,0 +1,20 @@
+Name = Louise Lalescu
+iD = “2056821”
+SummerPlans = []
+SummerPlans.append = (“Sleep”)
+SummerPlans.append = (“Turn Up”)
+SummerPlans.append = (“Drive to ASU daily”)
+ 
+Print (“My name is “ + name + “.”)
+Print (“My name is “ + iD + “.”)
+ 
+Print (name[0])
+Print (name[1])
+Print (name[2])
+Print (name[3])
+Print (name[4])
+Print (name[5])
+ 
+Print (“My name all capitalized is  “ + name.upper()  + “.”)
+Print (“This summer, I plan to  “ + (‘, ‘.join(summerPlans))  + “.”)
+ 
